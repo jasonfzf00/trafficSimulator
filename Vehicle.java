54f5635cd2477a53c7e1a86df73149980b55764d
zffu12@gmail.com <zffu12@gmail.com>
2024-01-22 21:05:45 -0500

    Add Vehicle.java

---
+++ Vehicle.java
@@ -0,0 +1,39 @@
+/**
+ * @author Jason Fu     01/22/2024
+ */
+
+package trafficSimulator;
+
+/**
+ * This class serve as an interface and should not create an object of Vehicle.
+ */
+public class Vehicle {
+
+    int speed;
+    int direction;
+    int locationX;
+    int locationY;
+
+    public Vehicle(){}
+
+    /**
+     * 
+     * @param toSpeed set speed to toSpeed
+     */
+    public void accelerate(int toSpeed){}
+
+    /**
+     * 
+     * @param toSpeed set speed to toSpeed
+     */
+    public void decelerate(int toSpeed){}
+
+    /**
+     * 
+     * @param direction: 0 - Left; 1 - Right
+     * @param angle: We will assume it is the multiple of 90 degree for now, 180 e.g.
+     * e.g. (0, 90) indicates going straight.
+     */
+    public void makeTurn(int direction, int angle){}
+
+}
