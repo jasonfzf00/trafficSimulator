600f4efd3b3def4e66649b6d70e8436a8553e3a2
zffu12@gmail.com <zffu12@gmail.com>
2024-01-30 10:32:14 -0500

    Update changes to Dynamic items

--- src/trafficsimulator/road/roaditem/dynamicroaditem/Truck.java
+++ src/trafficsimulator/road/roaditem/dynamicroaditem/Truck.java
@@ -4,19 +4,10 @@ public class Truck extends Vehicle{
 
     private int loadWeight;
 
-    Truck(){}
-    Truck(int weight){}
-    Truck(int xAxis, int yAxis){}
-    Truck(int speed, int xAxis, int yAxis){}
-    Truck(int speed, int direction, int xAxis, int yAxis, int weight){}
+    public Truck(){}
+    public Truck(int weight){}
+    public Truck(int speed, int direction, int xAxis, int yAxis, int weight){}
 
-    @Override
-    public void makeTurn(int direction, int angle){}
-
-    /**
-     * 
-     * @param weight: set loadWeight to weight
-     */
     public void setLoadWeight(int weight){}
 
     public int getLoadWeight(){return loadWeight;}
