6c88766022dc770ed85ab9fbe966786e81b94825
zffu12@gmail.com <zffu12@gmail.com>
2024-01-30 00:20:19 -0500

    Add testing files

---
+++ src/trafficsimulatortest/test.java
@@ -0,0 +1,10 @@
+package src.trafficsimulatortest;
+
+import src.trafficsimulator.road.roaditem.dynamicroaditem.*;
+
+public class test {
+    
+    public static void main(String[] args){
+        System.out.println("Hi");
+    }   
+}
